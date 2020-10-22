25b7b818869bb8f2d5967a4b4611e9be3374c597
Kshitij Sawhney <59340059+kshitijsaw@users.noreply.github.com>
2020-10-22 22:19:53 +0530

    Initial commit for Continent centric methods

--- src/Continent.java
+++ src/Continent.java
@@ -1,2 +1,46 @@
+import java.util.ArrayList;
+
+/**Describing a Continent on the map in a game of RISK
+ * @author  Kshitij Sawhney
+ * @version  10 / 22 / 2020
+ */
 public class Continent {
+    /**Name of the continent*/
+    private String name;
+
+    /**List of countries within the continent*/
+    private ArrayList<Country> countryList;
+
+    /**Number of troops received as bonus for full ownership*/
+    private int bonusArmyValue;
+
+    /**
+     *3 param Constructor for Continent
+     * @param name name of the continent
+     * @param countryList list of contained countries
+     * @param bonusArmyValue troop bonus
+     */
+    public Continent(String name, ArrayList<Country> countryList,int bonusArmyValue){
+        this.name=name;
+        this.countryList=countryList;
+        this.bonusArmyValue=bonusArmyValue;
+    }
+
+    /**Getter for name
+     * @return name of the continent*/
+    public String getName() {
+        return name;
+    }
+
+    /**Getter for bonusArmyValue
+     * @return number of bonus troops*/
+    public int getBonusArmyValue() {
+        return bonusArmyValue;
+    }
+
+    /**Getter for countryList
+     * @return list of countries contained within the continent*/
+    public ArrayList<Country> getCountryList() {
+        return countryList;
+    }
 }
--- src/RiskModel.java
+++ src/RiskModel.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+
 public class RiskModel {
     public static void main(String[] args) {
         CommandParser cp = new CommandParser();
-        Continent con = new Continent();
-        Country country = new Country("Bolivia");
+        Country country = new Country("Canada");
+        ArrayList<Country> countrylist = new ArrayList<>();
+        countrylist.add(country);
+        Continent con = new Continent("North America", countrylist,69);
         Player pl = new Player("Kevin", 0);
         System.out.println(cp);
         System.out.println(con);
