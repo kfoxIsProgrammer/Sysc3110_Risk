60fd2a9abd14d32299c7c9568015ff721e630c45
Kevin Fox <39808977+kfoxIsProgrammer@users.noreply.github.com>
2020-10-25 12:38:15 -0400

    Battle Object added

---
+++ src/BattleObject.java
@@ -0,0 +1,36 @@
+public class BattleObject {
+    private Country attackingCountry;
+    private Country defendingCountry;
+    private int attackingArmy;
+    private int defendingArmy;
+    private boolean didAttackerWin;
+
+
+    public BattleObject(Country attacker, Country defender, int attackingArmy, int defendingArmy, boolean didAttackerWin){
+        this.attackingCountry = attacker;
+        this.defendingCountry = defender;
+        this.attackingArmy = attackingArmy;
+        this.defendingArmy = defendingArmy;
+        this.didAttackerWin = didAttackerWin;
+    }
+
+    public Country getAttackingCountry() {
+        return attackingCountry;
+    }
+
+    public Country getDefendingCountry() {
+        return defendingCountry;
+    }
+
+    public int getAttackingArmy() {
+        return attackingArmy;
+    }
+
+    public int getDefendingArmy() {
+        return defendingArmy;
+    }
+
+    public boolean isDidAttackerWin() {
+        return didAttackerWin;
+    }
+}
