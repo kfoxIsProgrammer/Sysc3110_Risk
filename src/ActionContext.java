776eb7696f0e9d60300c37eeee919148f15532fb
Omar <omarhashmi@cmail.carleton.ca>
2020-11-04 17:27:48 -0500

    Created action contexts for all phases and created phase enum

---
+++ src/ActionContext.java
@@ -0,0 +1,22 @@
+/**
+ * Information container for the model to pass to the view
+ *
+ * @author Omar Hashmi
+ * @version 11.04.2020
+ */
+
+public abstract class ActionContext {
+    /** The current phase **/
+    public final Phase phase;
+    /** The current player **/
+    public final Player player;
+    /** The countries on the map that should be highlighted **/
+    public final Country highlightedCountries[];
+
+    /** Constructor for ActionContext **/
+    ActionContext(Phase phase, Player player, Country[] highlightedCountries){
+        this.phase=phase;
+        this.player=player;
+        this.highlightedCountries=highlightedCountries;
+    }
+}
