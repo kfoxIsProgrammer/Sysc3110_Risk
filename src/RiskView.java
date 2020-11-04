d71a37576617002f268dc8baea75b0c42d98b934
Kshitij Sawhney <59340059+kshitijsaw@users.noreply.github.com>
2020-11-05 04:07:54 +0530

    created optionPane WIP different layuots for phases

--- src/RiskView.java
+++ src/RiskView.java
@@ -25,101 +25,142 @@ public class RiskView extends JFrame {
     private JPanel mapPanel; //JLayeredPane needs the parent Container to have a null Layout Manager
     private JLayeredPane mapLayeredPane;
     private JPanel infoPanel;
+    private JPanel optionPanel; // cardLayout with other panels that change based on the current phase
+    private CardLayout cardLayout;
+    private JPanel attackSelectAttackerPanel;
+    private JPanel attackSelectDefenderPanel;
 
-    private String path = "map.png";
-    private BufferedImage mapImage= ImageIO.read(new File(path));
+    private String mapImagePath ;//= "map.png";
+    private BufferedImage mapImage;
 
     private JTextArea infoArea;
     private JLabel mapImageLabel;
-    private JLabel currentPhase;
+    private String currentPhase;
     private JButton skipPhase;
 
     public RiskView(RiskModel riskmodel) throws IOException {
-        setTitle("Risk - GLobal Domination");
+        riskController = new RiskController(riskmodel);
+        mapImagePath = riskController.getMapImagePath();
+        setTitle(riskController.getGameTitle());
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new GridBagLayout());
 
         gbc.fill = GridBagConstraints.NONE;
-        gbc.anchor=GridBagConstraints.NORTHWEST;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
         gbc.gridx = 0;
         gbc.gridy = 0;
-        add(mapPanel(),gbc);
+        add(mapPanel(riskController), gbc);
         /*ensures mapPanel is loaded into the frame so dimension information can be used by
         for correctly placing other panels
          */
         pack();
 
-        gbc.anchor=GridBagConstraints.NORTHEAST;
+
+         // JTextArea in the top-right that gives constant information about the current state of the game like "player X's turn"
+        gbc.anchor = GridBagConstraints.NORTHEAST;
         gbc.gridx = 1;
         gbc.gridy = 0;
 
-        add(infoPanel(mapPanel.getHeight()),gbc);
+        add(infoPanel(mapPanel.getHeight()), gbc);
 
+        //JPanel in the bottom-right which changes with every move and gives next options at each successive update
+        gbc.anchor = GridBagConstraints.SOUTHEAST;
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+
+        add(optionPanel(mapPanel.getHeight()),gbc);
         pack();
         setResizable(false);
         setVisible(true);
 
     }
-    private JPanel mapPanel() throws IOException {
-        mapPanel = new MapPanel();
-        mapPanel.setBorder(new EtchedBorder(EtchedBorder.RAISED));
-        mapImage = ImageIO.read(new File(path));
 
-        mapPanel.setPreferredSize(new Dimension(mapImage.getWidth(),mapImage.getHeight()));
+    private JPanel optionPanel(int height) {
+        optionPanel = new JPanel();
+        cardLayout = new CardLayout();
+        optionPanel.setLayout(cardLayout);
+        optionPanel.add(attackSelectAttackerPanel(),"attackSelectAttackerPanel");
+        optionPanel.add(attackSelectDefenderPanel(),"attackSelectDefenderPanel");
+        /*for future use
+        *optionPanel.add(troopMoverPanelSelectSource(),"troopMoverPanelSelectSource");
+        * *optionPanel.add(troopMoverPanelSelectDestination(),"troopMoverPanelSelectDestination");
+        * optionPanel.add(deployPanelSelectDestination()."deployPanelSelectDestination")
+         */
+
+        cardLayout.show(attackSelectAttackerPanel,"attackSelectAttackerPanel");   //for now game starts in attack phase
+        return optionPanel;
+    }
+
+    private JPanel attackSelectAttackerPanel() {
+        attackSelectAttackerPanel = new JPanel();
+        attackSelectAttackerPanel.add(new JLabel("Select attacking country"));
+        return attackSelectAttackerPanel;
+    }
+
+    private JPanel mapPanel(RiskController riskController) throws IOException {
+        mapPanel = new MapPanel(riskController);
+        mapPanel.setBorder(new EtchedBorder(EtchedBorder.RAISED));
+        mapImage = ImageIO.read(new File(mapImagePath));
+
+        mapPanel.setPreferredSize(new Dimension(mapImage.getWidth(), mapImage.getHeight()));
 
         mapLayeredPane = new JLayeredPane();
         //inserting into Layered pane
         mapImageLabel = new JLabel(new ImageIcon(mapImage));
-        mapImageLabel.setBounds(0,0,mapImage.getWidth(),mapImage.getHeight());
+        mapImageLabel.setBounds(0, 0, mapImage.getWidth(), mapImage.getHeight());
         mapLayeredPane.add(mapImageLabel, Integer.valueOf(1));
 
         mapPanel.setLayout(null);
         mapLayeredPane.setLayout(null);
-        mapLayeredPane.setBounds(0,0,mapImage.getWidth(),mapImage.getHeight());
+        mapLayeredPane.setBounds(0, 0, mapImage.getWidth(), mapImage.getHeight());
 
         mapPanel.add(mapLayeredPane);
 
         return mapPanel;
     }
 
-    private JPanel infoPanel(int height){
+    private JPanel infoPanel(int height) {
         infoPanel = new JPanel();
-        infoPanel.setLayout(new BoxLayout(infoPanel,BoxLayout.Y_AXIS));
+        infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
         infoPanel.setBorder(new LineBorder(Color.black));
 
         infoArea = new JTextArea();
         infoArea.setFocusable(false);
         infoArea.setLineWrap(true);
         infoArea.setWrapStyleWord(true);
-        ((DefaultCaret)infoArea.getCaret()).setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+        ((DefaultCaret) infoArea.getCaret()).setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
         JScrollPane JP = new JScrollPane(infoArea);
-        JP.setPreferredSize(new Dimension(200,(int)height/2));
-        infoPanel.add(JP,0);
+        JP.setPreferredSize(new Dimension(200, (int) height / 2));
+        infoPanel.add(JP, 0);
 
-        infoPanel.setSize(new Dimension(200,height));
+        infoPanel.setSize(new Dimension(200, height));
 
         return infoPanel;
     }
 
     public void boardUpdate(RiskModel riskModel){
-
-    }
-
-    public static void main(String[] args) throws Exception{
-        new RiskView();
-        //TODO create board render based on currentl game state
+        //TODO update board with every move
+        currentPhase = riskModel.getcurrentPhase();
+        switch(currentPhase){
+            case "attackSelectAttacker":
+                cardLayout.show(attackSelectAttackerPanel,"attackSelectAttackerPanel");
+                break;
+            case "attackSelectDefender":
+                cardLayout.show(attackSelectDefenderPanel,"attackSelectDefenderPanel");
+                higlightAdjacentCountries();
+                break;
+        }
     }
 }
 
-
 class MapPanel extends JPanel{
-    public MapPanel(){
+    public MapPanel(RiskController controller){
         addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
                 System.out.println(e.getX()+" "+e.getY());
-                RiskController.countrySelected(e);
+                controller.countrySelected(e);
             }
         });
     }
