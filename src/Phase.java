4f333752471746318f35937876bb0ad8c18965e7
Kshitij Sawhney <59340059+kshitijsaw@users.noreply.github.com>
2020-11-06 05:53:12 +0530

    RiskView updated, added extra phases for confirmation

--- src/Phase.java
+++ src/Phase.java
@@ -10,12 +10,16 @@ public enum Phase {
     DEPLOY_DST,
     /** Deploy phase selecting number of troops to deploy **/
     DEPLOY_ARMY,
+    /**Deploy phase confirmation**/
+    DEPLOY_CONF,
     /** Attack phase selecting a source country **/
     ATTACK_SRC,
     /** Attack phase selecting a destination country **/
     ATTACK_DST,
     /** Attack phase selecting number of troops to send **/
     ATTACK_ARMY,
+    /**Attack phase confirmation**/
+    ATTACK_CONF,
     /** Attack phase displaying result of battle **/
     ATTACK_DICE,
     /** Fortify phase selecting a source country **/
@@ -23,5 +27,7 @@ public enum Phase {
     /** Fortify phase selecting a destination country **/
     FORTIFY_DST,
     /** Fortify phase selecting number of troops to send **/
-    FORTIFY_ARMY
+    FORTIFY_ARMY,
+    /**Fortify phase confirmation**/
+    FORTIFY_CONF
 }
