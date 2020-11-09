bedc6f002aaee7cbb217153348c36061bc8f360e
dkout <DimitryKoutchine@cmail.carleton.ca>
2020-11-09 12:58:00 -0500

    added more testcases

--- src/RiskModelTest.java
+++ src/RiskModelTest.java
@@ -257,10 +257,10 @@ public class RiskModelTest extends TestCase {
     public void testCountriesArrayOrder(){
         RiskModel test = new RiskModel(twoPlayers);
 
-        String[] toTest = {"Alaska","Alberta ","Central America","Eastern United States","Greenland","Northwest Territory",
-                "Ontario ","Quebec ","Western United States","Argentina","Brazil","Peru","Venezuela","Great Britain ","Iceland",
-                "Northern Europe","Scandinavia","Southern Europe","Ukraine ","Western Europe","Congo ","East Africa","Egypt",
-                "Madagascar","North Africa","South Africa","Afghanistan","China","India ","Irkutsk","Japan","Kamchatka","Middle East","Mongolia","Siam ","Siberia","Ural ",
+        String[] toTest = {"Alaska","Alberta","Central America","Eastern United States","Greenland","Northwest Territory",
+                "Ontario","Quebec","Western United States","Argentina","Brazil","Peru","Venezuela","Great Britain","Iceland",
+                "Northern Europe","Scandinavia","Southern Europe","Ukraine","Western Europe","Congo","East Africa","Egypt",
+                "Madagascar","North Africa","South Africa","Afghanistan","China","India","Irkutsk","Japan","Kamchatka","Middle East","Mongolia","Siam ","Siberia","Ural ",
                 "Yakutsk","Eastern Australia","Indonesia","New Guinea","Western Australia"};
         for(int i = 0; i < toTest.length; i++){
             assertEquals(toTest[i],test.countries[i].getName());
