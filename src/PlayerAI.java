4c467749693357446630dc3ab440589dec2b2234
dkout <DimitryKoutchine@cmail.carleton.ca>
2020-11-20 22:10:28 -0500

    added method to assign bonus troops

--- src/PlayerAI.java
+++ src/PlayerAI.java
@@ -140,4 +140,18 @@ public class PlayerAI extends Player{
                 break;
         }
     }
+    private void allocateBonusTroops(Player user){
+        boolean willTroopsBeAssigned;
+        for (Continent cont: continents){
+            willTroopsBeAssigned = true;
+            for(Country count: cont.countryList){
+                if (!user.countries.contains(count)){ willTroopsBeAssigned = false;}
+
+            }
+            if(willTroopsBeAssigned){
+                user.addArmyToAllocate(cont.bonusArmyValue);
+            }
+        }
+
+    }
 }
